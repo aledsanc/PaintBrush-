675386c4c67b261f9231a3add02dab64f6a3b938
aledsanc <68257871+aledsanc@users.noreply.github.com>
2021-07-12 22:44:42 -0700

    new

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 let config = {
-    type: Phaser.ARCADE,
+    type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [Menu, Play, GameOver, Help]
+    scene: [Menu, Play, GameOver, Help],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    }
 }
 
 let game = new Phaser.Game(config);
